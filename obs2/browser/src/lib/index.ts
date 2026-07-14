c68689fd12ca32a7a022fb5713549b85d7c9f138
acheronfail <acheronfail@gmail.com>
2026-07-14 14:43:43 +0930

    custom selects for linux CEF (#77)

--- obs2/browser/src/lib/index.ts
+++ obs2/browser/src/lib/index.ts
@@ -9,6 +9,8 @@ export {
 	settings
 } from './settings.svelte';
 export type { RecordingOptions, Settings, UpdateCheckInterval } from './settings.svelte';
+export { default as Select } from './Select.svelte';
+export type { SelectOption } from './Select.svelte';
 
 export const VERSION = import.meta.env.VITE_GE_PLUGIN_VERSION ?? '0.0.0-unknown';
 export const IS_DEV = import.meta.env.VITE_GE_PLUGIN_VERSION
